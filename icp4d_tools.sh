d9f0f4be2baffc681b014479d39379c2b5a812d6
root <root@icpd231-7-1-master-1.fyre.ibm.com>
2018-10-22 08:49:36 -0700

    Initial changes to command line argument

--- icp4d_tools.sh
+++ icp4d_tools.sh
@@ -19,45 +19,49 @@ Print_Usage() {
   echo "Usage:"
   echo "$0 [OPTIONS]"
   echo -e "\n  OPTIONS:"
-  echo "      -pre: Run pre-installation requirements checker (CPU, RAM, and Disk space, etc.)"
-  echo "      -health: Run post-installation cluster health checker"
-  echo "      -resource: Check avaialble resoirce on cluster"
-  echo "      -l, --log: Run log collection tool to collect log files from critical pods/containers"
+  echo "      -i, --interactive: Run the tool in an interactive mode"
+  echo "      -p, --preinstall: Run pre-installation requirements checker (CPU, RAM, and Disk space, etc.)"
+  echo "      -h, --health: Run post-installation cluster health checker"
+  echo "      -c, --collect=smart|standard: Run log collection tool to collect diagnostics and logs files from every pod/container. Default is smart"
+  echo "      -lt, --logtail=-1|K Capture last K lines or all when -1, works with stanard collect mode" 
   echo "      -h, --help: Prints this message"
   echo -e "\n  EXAMPLES:"
-  echo "      $0 -pre"
-  echo "      $0 -health"
-  echo "      $0 --log"
+  echo "      $0 -preinstall"
+  echo "      $0 --health"
+  echo "      $0 --collect=smart"
   echo
   exit 0
 }
 
 Selected_Option() {
-    TEMP=`getopt -o plh: --long pre,health,log,help: -n 'icptools.sh' -- "$@"`
-
-    if [ $? != 0 ] ; then 
-        echo "error processing options..." >&2 
-        exit 1
-    fi
-
-    eval set -- "$TEMP"
-
-    TASK=""
-    while true; do
-        case "$1" in
-            -h | --help ) TASK=Print_Usage; shift ;;
-            -p | --pre  ) TASK=Prereq_CHK; shift ;;
-            -l | --log ) TASK=Collect_Logs; shift ;;
-            --health ) TASK=Health_CHK; shift ;;
-            --resource ) TASK=Resource_CHK; shift ;;
-            -- ) shift; break ;;
-            * ) break ;;
-        esac
-    done
-    if [ ! -z "$TASK" ]; then
-        $TASK $@
-        exit $?
-    fi
+  #entry to the tool
+	
+  #Switch between Wizard or Param driven tooling 
+		
+  if [ ! -z ${_ICP_INTERACTIVE} ]; then
+     echo " switching to Wizard Based"
+     ICP_Tools_Menu
+     exit
+  fi
+	
+  if [ ! -z ${_ICP_HELP} ]; then
+     Print_Usage;
+     exit
+  fi
+	
+  if [ ! -z ${_ICP_COLLECT} ]; then
+     Collect_Logs;
+  fi
+	
+	
+  if [ ! -z ${_ICP_HEALTH} ]; then
+     Health_CHK;
+     #Resource_CHK;
+  fi
+	
+  if [ ! -z ${_ICP_PREINSTALL} ]; then
+     Prereq_CHK;
+  fi
 }
 
 
@@ -67,6 +71,7 @@ setup() {
     export LOG_COLLECT_DIR=`pwd`"/log_collector"
     export PLUGINS=`pwd`"/log_collector/plugins"
     . $UTIL_DIR/util.sh
+    . $UTIL_DIR/get_params.sh
     . $LOG_COLLECT_DIR/icpd-logcollector-master-nodes.sh
 }
 
@@ -127,6 +132,27 @@ Collect_Component_Logs() {
         clean_up $logs_dir
 }
 
+
+Collect_DB2_Hang_Logs() {
+        export COMPONENT=db2
+        export logs_dir=`mktemp -d`
+        export LINE=500
+        export DB2POD=`kubectl get pod --all-namespaces --no-headers -o custom-columns=NAME:.metadata.name|grep db2`
+
+        cd $HOME_DIR
+        for cmd in `cat ./log_collector/component_sets/db2_hang_log.set`
+        do
+           . $PLUGINS/$cmd
+        done
+        local timestamp=`date +"%Y-%m-%d-%H-%M-%S"`
+        local archive_name="logs_"$$"_"$timestamp".tar.gz"
+        local output_dir=`mktemp -d -t icp4d_collect_log.XXXXXXXXXX`
+        build_archive $output_dir $archive_name $logs_dir "./"
+        echo Logs collected at $output_dir/$archive_name
+        clean_up $logs_dir
+}
+
+
 Resource_CHK(){
         echo -e "****************** \n";
         echo -e "Resources Usage at cluster level....\n"
@@ -147,8 +173,8 @@ Resource_CHK(){
 
 ICP_Tools_Menu() {
   while true; do
-    echo -e "Choose an option [1-6] \n";
-    options=("Pre-install checks for ICPD installation" "Health-check an installed ICPD cluster" "Collect diagnostics data for down pods" "Collect diagnostics data" "List Resource Usage" "Exit")
+    echo -e "Choose an option [1-7] \n";
+    options=("Pre-install checks for ICPD installation" "Health-check an installed ICPD cluster" "Collect diagnostics data for down pods" "Collect diagnostics data for DSX" "Collect diagnostics data for DB2" "List Resource Usage" "Exit")
     COLUMNS=12;
     select opt in "${options[@]}";
     do
@@ -159,8 +185,10 @@ ICP_Tools_Menu() {
     	  Health_CHK; break;;
         "Collect diagnostics data for down pods")
           Collect_Down_Pod_Logs; break;;
-        "Collect diagnostics data")
+        "Collect diagnostics data for DSX")
           Collect_Component_Logs; break;;
+        "Collect diagnostics data for DB2")
+          Collect_DB2_Hang_Logs; break;;
 	"List Resource Usage")
 	  Resource_CHK; break;;
         "Exit")
@@ -179,5 +207,5 @@ exitSCRIPT(){
 }
 
 setup $@
-Selected_Option $@
+#Selected_Option $@
 ICP_Tools_Menu
---
+++ util/get_params.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+#
+
+#
+# parses the argument list
+# when it finds a --var=name or -v or -v name
+# it creates a variable _ICP_{VAR} and sets its value to {NAME}
+# note that the created variable is in uppercase.
+# in your shell script, use:
+# . $ROOT/bin/get_params.sh
+# Short forms of arguments supported with the help of $INI_FILE map. 
+# A map (below $INI_FILE) is provided where shorts forms are mapped to the long forms.
+# Callee need to pass the key which is same as the section whose maps are to be refered.
+# for example, if the section in ini file is icp4dsupport use:
+# . $ROOT/bin/get_params.sh icp4dsupport
+# [--var=yes] can be written as -v [assuming the INIT_FILE this map is provided]
+# which is translated to _ICP_VAL=yes
+# [-v val] would be translated to _ICP_VAL_=val
+#
+# ex: get_params.sh --confirm_db=yes --name=acme
+#     sets 2 variables: _ICP_CONFIRM_DB=yes, _ICP_NAME=acme
+#     get_params.sh -v val -c sectionkey [assume in map v=expanedV, c=expandedC]
+#     sets 2 varables : _ICP_EXPANDEDV=val, _ICP_EXPANDEDC=yes
+# use --debug=yes to see some debugging info
+echo $UTIL_DIR
+if [ -f $UTIL_DIR/conf/param_maps.ini ]
+then
+        INI_FILE=$UTIL_DIR/conf/param_maps.ini
+else
+        
+                echo ""
+                echo "Please check whether environment is installed and setup correctly."
+                echo "File param_maps.ini does not exist in $TOP/bin/conf"
+                echo "If this is a horizontal cluster env, variable ICP_CONFIG_DIR must"
+                echo "specify alternate directory containing configuration files from $TOP/bin/conf"
+                echo ""
+                exit 1
+       
+fi
+
+args=( "$@" )
+cnt="${#args[@]}"
+ptr=0
+
+for _VAR in "$@"; do
+flag=0
+case ${_VAR} in
+        --*=* )
+            _VALUE=`echo ${_VAR}|cut -f2 -d "="`
+            #_NAME=`echo ${_VAR}|cut -f1 -d "="|cut -f3 -d "-"|tr '[:lower:]' '[:upper:]'` 
+            #_NAME=`echo ${_VAR}|cut -f1 -d "="|cut -f3 -d "-"|tr '[a-z]' '[A-Z]'`
+            _NAME=`echo ${_VAR}|cut -f1 -d "="|cut -f3 -d "-"|tr 'abcdefghijklmnopqrstuvwxyz' 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'`
+            _NEW_NAME="_ICP_${_NAME}"
+            flag=1
+            ptr=$((ptr+1))
+   ;; 
+       --* )
+            _NAME=`echo ${_VAR}|cut -f3 -d "-"|tr 'abcdefghijklmnopqrstuvwxyz' 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'`
+            _NEW_NAME="_ICP_${_NAME}"
+            _VALUE="yes"
+            flag=1
+            ptr=$((ptr+1)) 
+   ;;
+       -[^-]* )
+            _NAME=`echo ${_VAR}|cut -f2 -d "-"`
+            echo $_NAME
+            INI_SECTION="${args[$((cnt-1))]}"
+            eval `sed -e 's/[[:space:]]*\=[[:space:]]*/=/g' \
+                          -e 's/;.*$//' \
+                          -e 's/[[:space:]]*$//' \
+                          -e 's/^[[:space:]]*//' \
+                          -e "s/^\(.*\)=\([^\"']*\)$/\1=\"\2\"/" \
+                          < $INI_FILE  | sed -n -e "/^\[$INI_SECTION\]/,/^\s*\[/{/^[^;].*\=.*/p;}"`
+            _NAME=`eval echo \\$${_NAME}|tr 'abcdefghijklmnopqrstuvwxyz' 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'`
+            echo $_NAME
+            _NEW_NAME="_ICP_${_NAME}"
+            ptr=$((ptr+1))
+             _VALUE="${args[$((ptr))]}"
+            if [ `echo ${_VALUE} | grep -e "-" -e "$INI_SECTION"` ]; then
+            _VALUE="yes"
+            else
+              ptr=$((ptr+1))
+            fi
+            flag=1
+    
+   ;;
+  esac
+           # do not assign value twice (debug mode looks nicer with nested calls :-)
+            if [ ! "`eval echo \\$${_NEW_NAME}`" = "${_VALUE}" -a $flag -eq 1 ]; then
+                if [ "${_ICP_DEBUG}" = "yes" ]; then
+                    echo "[setting ${_NEW_NAME}=${_VALUE}]"
+                fi
+                eval "${_NEW_NAME}=\"${_VALUE}\""
+            fi
+done
